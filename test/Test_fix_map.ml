5eef4a70e01ef981d3d50078b68a71a6d0ac463b
Qexat <43090614+qexat@users.noreply.github.com>
2025-05-09 13:38:07 +0200

    test `Fix.With_map`: add `Linked` (from `Option`)

--- test/Test_fix_map.ml
+++ test/Test_fix_map.ml
@@ -1,13 +1,21 @@
 open Obric
 module Tree = Fix.With_map (List)
+module Linked = Fix.With_map (Option)
 
-let my_list : int Tree.t = Fix (3, [ Fix (5, [ Fix (2, []) ]); Fix (4, []) ])
+let my_tree : int Tree.t = Fix (3, [ Fix (5, [ Fix (2, []) ]); Fix (4, []) ])
+let my_list : string Linked.t = Fix ("43", Some (Fix ("17", Some (Fix ("0", None)))))
 
 (* tests *)
 
 let () =
   Test_manager.register "map over fix-list" (fun () ->
     assert (
-      Tree.map Int.to_string my_list
+      Tree.map Int.to_string my_tree
       = Fix ("3", [ Fix ("5", [ Fix ("2", []) ]); Fix ("4", []) ])))
+;;
+
+let () =
+  Test_manager.register "map over fix-option" (fun () ->
+    assert (
+      Linked.map int_of_string my_list = Fix (43, Some (Fix (17, Some (Fix (0, None)))))))
 ;;
